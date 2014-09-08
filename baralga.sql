f6a9adfa148c977d9eefdbc787b098e31719c928
Jan Stamer <jan.stamer@red6-es.de>
2014-09-08 20:34:06 +0200

    Added PK

--- baralga.sql
+++ baralga.sql
@@ -1,15 +1,21 @@
 
 CREATE TABLE project (
-     id           INTEGER,
-     title        VARCHAR(255),
-     description  VARCHAR(4000),
-     active       TINYINT
+  id           INTEGER,
+  title        VARCHAR(255),
+  description  VARCHAR(4000),
+  active       TINYINT, 
+  CONSTRAINT PK_project PRIMARY KEY (
+    id
+  )
 );
 
 CREATE TABLE activity (
-     id           INTEGER,
-     description  VARCHAR(4000),
-     act_start        TIMESTAMP,
-     act_end          TIMESTAMP,
-     project_id   TINYINT
+  id           INTEGER,
+  description  VARCHAR(4000),
+  act_start    TIMESTAMP,
+  act_end      TIMESTAMP,
+  project_id   TINYINT,
+  CONSTRAINT activity PRIMARY KEY (
+    id
+  )
 );
